e61e0005abd46c755dc2b976d8244b2d26d9eb4d
Ben Cook <b@bencook.ca>
2015-07-20 17:57:52 -0700

    Create scripts dir for scripts

---
+++ scripts/langs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clang --version | head -n 1
+gcc-5 --version | head -n 1
+gfortran --version | head -n 1
+ghc --version
+ocaml -version
+php --version | head -n 1
+python --version
+python3 --version
+R --version | head -n 1
+echo "node $(node --version)"
+ruby --version
+perl --version | head -n 2 | tail -n 1
+java -version | head -n 1
+lein --version
+rustc --version
+erl -version
