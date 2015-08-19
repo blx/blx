960f78353ea94d35e5bee22387baba7422feb3a1
Ben Cook <b@bencook.ca>
2015-08-19 00:08:58 -0700

    alphabetize langs.sh output

--- scripts/langs.sh
+++ scripts/langs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 clang --version | head -n 1
+erl -version
 gcc-5 --version | head -n 1
 gfortran --version | head -n 1
-ghc --version
 go version
+ghc --version
+java -version | head -n 1
+lein --version
+echo "node $(node --version)"
 ocaml -version
+perl --version | head -n 2 | tail -n 1
 php --version | head -n 1
 python --version
 python3 --version
 R --version | head -n 1
-echo "node $(node --version)"
 ruby --version
-perl --version | head -n 2 | tail -n 1
-java -version | head -n 1
-lein --version
 rustc --version
-erl -version
