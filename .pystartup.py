95e7e925696a17fd8c42636cc284a8a12c7c6e13
Ben Cook <b@bencook.ca>
2016-02-04 20:42:14 -0800

    [python] Add utility fns and imports to pystartup

--- .pystartup.py
+++ .pystartup.py
@@ -1,6 +1,12 @@
+"""PYSTARTUP
+Everything in this module will be executed/made available whenever a REPL is
+launched."""
+
 import re
+import os
 import itertools as it
 import operator as op
+from pprint import pprint
 from functools import partial
 p = partial
 
@@ -10,3 +16,22 @@ def doc(fn):
     Prints the docstring for `fn`, if there is one."""
 
     print(fn.__doc__)
+
+identity = lambda x: x
+inc = lambda x: x + 1
+dec = lambda x: x - 1
+
+juxt = lambda *fns: lambda *a, **kw: [f(*a, **kw) for f in fns]
+
+def iterate(f, x):
+    while True:
+        yield x
+        x = f(x)
+
+def slurp(f):
+    with open(f):
+        return f.read()
+
+def spit(f, txt):
+    with open(f, 'w'):
+        f.write(txt)
