f4720b260fb4afa00e50c3e2c34ebb2d1e0f1c2a
Ben Cook <b@bencook.ca>
2016-03-31 23:36:43 -0700

    [clojure] load cheshire in repl instead of data.json

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-exec "0.3.6"]]
         :dependencies [[com.cemerick/pomegranate "0.3.1"]
-                       [org.clojure/data.json "0.2.6"]]}}
+                       [cheshire "5.5.0"]]}}
