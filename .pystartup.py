55f430b78148f03c3398a8605f3f1d68f32e8ab9
Ben Cook <b@bencook.ca>
2016-03-31 23:38:33 -0700

    [python] improve pystartup: add comp, fix slurp, use useful docstrings

--- .pystartup.py
+++ .pystartup.py
@@ -1,6 +1,7 @@
-"""PYSTARTUP
-Everything in this module will be executed/made available whenever a REPL is
-launched."""
+# PYSTARTUP
+#
+# Everything in this module will be executed/made available whenever a REPL is
+# launched.
 
 import re
 import os
@@ -8,8 +9,12 @@ import itertools as it
 import operator as op
 from pprint import pprint
 from collections import namedtuple, Counter
+from functools import reduce
 from functools import partial
 p = partial
+
+
+### REPL stuff
 
 # Autocompletion
 try:
@@ -32,16 +37,37 @@ def doc(fn):
     Prints the docstring for `fn`, if there is one."""
     print(fn.__doc__)
 
+
+### Core prelude
+
 def identity(x): return x
 def inc(x):      return x + 1
 def dec(x):      return x - 1
 
+def is_pos(x):   return x > 0
+def is_neg(x):   return x < 0
+def is_zero(x):  return x == 0
+
 first  = op.itemgetter(0)
 second = op.itemgetter(1)
+
+def comp(*fns):
+    if not fns:
+        return identity
+
+    # :o
+    def _comped(*a, **kw):
+        x = fns[-1](*a, **kw)
+        for f in fns[-2::-1]:
+            x = f(x)
+        return x
+    _comped.__name__ = 'comp(%s)' % ', '.join(map(op.attrgetter('__name__'), fns))
+    return _comped
 
 def juxt(*fns):
     def _juxted(*a, **kw):
         return [f(*a, **kw) for f in fns]
+    _juxted.__name__ = 'juxt(%s)' % ', '.join(map(op.attrgetter('__name__'), fns))
     return _juxted
 
 def iterate(f, x):
@@ -53,10 +79,10 @@ def take(n, coll):
     return list(it.islice(coll, n))
 
 def slurp(f):
-    with open(f):
-        return f.read()
+    with open(f) as fp:
+        return fp.read()
 
 def spit(f, txt):
     """spit(f, txt): Writes `txt` to file at `f`"""
-    with open(f, 'w') as f:
-        f.write(txt)
+    with open(f, 'w') as fp:
+        fp.write(txt)
