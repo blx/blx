0befe3d8a3f33156b67532d90abc8734cb388256
Ben Cook <b@bencook.ca>
2016-05-03 21:39:46 -0700

    [script] add some fixup scripts (homebrew; python mysqldb)

---
+++ scripts/fix-python-mysqldb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# The error is like so:
+#
+# django.core.exceptions.ImproperlyConfigured: Error loading MySQLdb module: dlopen(/Users/ben/projects/dispatch/env/lib/python2.7/site-packages/_mysql.so, 2): Library not loaded: libmysqlclient.18.dylib
+#   Referenced from: /Users/ben/projects/dispatch/env/lib/python2.7/site-packages/_mysql.so
+#   Reason: image not found
+
+dotso='./env/lib/python2.7/site-packages/_mysql.so'
+
+install_name_tool -change libmysqlclient.18.dylib /usr/local/mysql/lib/libmysqlclient.18.dylib $dotso
