3b5722a0e4b80b465d49db4ab669180b16b6ac60
Ben Cook <b@bencook.ca>
2016-06-01 00:02:40 -0700

    [clojure] disable ultra for now for more informative stacktraces

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-exec "0.3.6"]
-                  [venantius/ultra "0.4.1"]]
+{:user {:plugins [[lein-exec "0.3.6"]]
+;                  [venantius/ultra "0.4.1"]]
         :dependencies [[com.cemerick/pomegranate "0.3.1"]
                        [cheshire "5.5.0"]]}}
