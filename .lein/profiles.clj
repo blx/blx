4c1b5811216f6368e18544f87a0f26e1a4197302
Ben Cook <b@bencook.ca>
2017-01-04 12:51:10 -0500

    [clojure] update global cheshire

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,3 @@
 {:user {:plugins [[lein-exec "0.3.6"]]
-;                  [venantius/ultra "0.4.1"]]
         :dependencies [[com.cemerick/pomegranate "0.3.1"]
-                       [cheshire "5.6.1"]]}}
+                       [cheshire "5.6.3"]]}}
