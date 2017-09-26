8d889c0e6e1a6ca875816533f0bc3d673b13f646
Ben Cook <b@bencook.ca>
2017-09-25 23:25:56 -0700

    [clj] add core.async to leiningen global profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-exec "0.3.6"]]
         :dependencies [[com.cemerick/pomegranate "0.3.1"]
-                       [cheshire "5.6.3"]]}}
+                       [cheshire "5.6.3"]
+                       [org.clojure/core.async "0.3.443"]]}}
