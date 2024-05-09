dd155a2eed448aa6ef903ceb51d550aa8da102a6
Ben Cook <b@bencook.ca>
2024-05-09 03:00:08 -0700

    [clj] remove old lein global overrides

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,2 @@
 {:user {:plugins [[lein-exec "0.3.6"]]
-        :dependencies [[com.cemerick/pomegranate "0.3.1"]
-                       [cheshire "5.6.3"]
-                       [org.clojure/core.async "0.3.443"]]}}
+        :dependencies [[com.cemerick/pomegranate "0.3.1"]]}}
